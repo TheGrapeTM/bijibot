401a18e39c9d73cd0f6325e3a795b1cfe2b7cf89
savierneoo <nxavier.17@ichat.sp.edu.sg>
2021-05-14 13:59:06 +0000

    added references

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-import telegram.ext
-import os
-
-API_KEY = os.getenv('API_KEY')
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
+import keys
 
 print('Bot started...')
 
 def start_command(update, context):
-    """
-    Starts the bot with /start
-    """
+    reply_keyboard = [['Talk', 'Photo', 'Send lub']]
+    update.message.reply_text(
+        #  'Hi! My name is Professor Bot. I will hold a conversation with you. '
+        'What would you like biji to do?'
+        'Send /cancel at any time to stop talking to me. But I\'ll be sad :(\n\n',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+    )
     pass
 
 def help_command(update, context):
@@ -31,12 +41,15 @@ def error(update, context):
     pass
 
 def main():
-    updater = Updater(API_KEY)
+    #  bot = telegram.Bot(keys.API_KEY)
+    updater = Updater(keys.API_KEY)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler('start', start_command))
-    dispatcher.add_handler(CommandHandler('start', help_command))
+    #  bot.send_message('Hello, type /start to start the program')
 
+    dispatcher.add_handler(CommandHandler('start', start_command))
+    #  dispatcher.add_handler(CommandHandler('help', help_command))
+    #
     dispatcher.add_error_handler(error)
 
     updater.start_polling()
